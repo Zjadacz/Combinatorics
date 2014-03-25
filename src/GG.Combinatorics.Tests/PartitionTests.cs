f0c73d1485d5f8deb74decf0760db515e8f5f86f
Zjadacz <jerzy.waldorff@gmail.com>
2014-03-25 01:26:02 +0000

    Partition class added.

---
+++ src/GG.Combinatorics.Tests/PartitionTests.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GG.Combinatorics.Tests
+{
+    [TestClass]
+    public class PartitionTests
+    {
+        [TestMethod]
+        public void NextPartitionOfEmptyListReturnsFalse()
+        {
+            // Given
+            var list = new List<int>();
+
+            // When
+            var result = NextPartition(list);
+
+            // Then
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void NextPartitionOfEmptyListDoesntUpdateInput()
+        {
+            // Given
+            var list = new List<int>();
+
+            // When
+            NextPartition(list);
+
+            // Then
+            Assert.AreEqual(0, list.Count);
+        }
+
+        [TestMethod]
+        public void NextPartitionOn1ReturnsFalse()
+        {
+            // Given
+            var list = new List<int> { 1 };
+
+            // When
+            var result = NextPartition(list);
+
+            // Then
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void NextPartitionOn1DoesntUpdateList()
+        {
+            // Given
+            var list = new List<int>{ 1 };
+            var expectedList = new List<int> { 1 };
+
+            // When
+            NextPartition(list);
+
+            // Then
+            CollectionAssert.AreEqual(expectedList, list);
+        }
+
+        [TestMethod]
+        public void NextPartitionOn2UpdatesListTo11()
+        {
+            // Given
+            var list = new List<int> { 2 };
+            var expectedList = new List<int> { 1, 1 };
+
+            // When
+            NextPartition(list);
+
+            // Then
+            CollectionAssert.AreEqual(expectedList, list);
+        }
+
+        public void NextPartitionOn2ReturnsTrue()
+        {
+            // Given
+            var list = new List<int> { 2 };
+            var expectedList = new List<int> { 1, 1 };
+
+            // When
+            var result = NextPartition(list);
+
+            // Then
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void NextPartitionOn12UpdatesListTo111()
+        {
+            // Given
+            var list = new List<int> { 1, 2 };
+            var expectedList = new List<int> { 1, 1, 1 };
+
+            // When
+            NextPartition(list);
+
+            // Then
+            CollectionAssert.AreEqual(expectedList, list);
+        }
+        
+        [TestMethod]
+        public void NextPartitionOn14UpdatesListTo23()
+        {
+            // Given
+            var list = new List<int> { 1, 4 };
+            var expectedList = new List<int> { 2, 3 };
+
+            // When
+            NextPartition(list);
+
+            // Then
+            CollectionAssert.AreEqual(expectedList, list);
+        }
+
+        [TestMethod]
+        public void NextParititionOn1113UpdatesListTo222()
+        {
+            // Given
+            var list = new List<int> { 1, 1, 1, 3 };
+            var expectedList = new List<int> { 2, 2, 2 };
+
+            // When
+            NextPartition(list);
+
+            // Then
+            CollectionAssert.AreEqual(expectedList, list);
+        }
+
+        public bool NextPartition(List<int> input)
+        {
+            return new Partition().Next(input);
+        }
+    }
+}
---
+++ src/GG.Combinatorics/Partition.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GG.Combinatorics
+{
+    public class Partition
+    {
+        /// <summary>
+        /// Example:
+        /// {6}, {1,5}, {2,4}, {1,1,4}, {1,2,3}, {1,1,1,3}, {2,2,2}, {1,1,2,2}, {1,1,1,1,2,2}, {1,1,1,1,1,1}
+        /// Algorithm:
+        /// a) if first number is greather than 1, subtract one from it insert 1 at the beginning
+        /// b) look up for number greather than 1 and 
+        /// b1) if number is equal two subtract one from it and add insert one as predecessor
+        /// b2) if number is more than two subtract one from it and add one to predecessor, aggregate to left
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public bool Next(List<int> input)
+        {
+            if (input.Count == 0 || input.Last() == 1)
+            {
+                return false;
+            }
+
+            if (input[0] > 1)
+            {
+                input[0]--;
+                input.Insert(0, 1);
+                return true;
+            }
+
+            int i = 1;
+            while (i < input.Count)
+            {
+                if (input[i] > 1)
+                {
+                    input[i]--;
+                    if (input[i] == 1)
+                    {
+                        input.Insert(i, 1);
+                    }
+                    else
+                    {
+                        input[i - 1]++;
+                        AggregateLeft(input, i);
+                    }
+                    return true;
+                }
+                i++;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// For given position in list aggregates all values before the position 
+        /// to the greatest possible combination, for example (position indicated with quotation marks):
+        /// {1,1,"3"}=>{2,3}
+        /// {1,1,1,"3"}=>{3,3}
+        /// {1,1,1,1,"2",5}=>{2,2,2,5}
+        /// </summary>
+        /// <param name="input">Input list to aggregate values before given index</param>
+        /// <param name="index">Position to which aggregation should take place</param>
+        private void AggregateLeft(List<int> input, int index)
+        {
+            int value = input[index];
+            int sum = input.Take(index).Sum();
+            int division = sum / value;
+            int rest = sum % value;
+
+            for (int i = 0; i < index; i++)
+            {
+                input.RemoveAt(0);
+            }
+
+            for (int i = 0; i < division; i++)
+            {
+                input.Insert(0, value);
+            }
+
+            if (rest > 0)
+            {
+                input.Insert(0, rest);
+            }
+        }
+    }
+}
