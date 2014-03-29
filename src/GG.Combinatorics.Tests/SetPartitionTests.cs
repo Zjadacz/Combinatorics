606f308cb9802a9f188b02102f4dbb53f134460c
Zjadacz <jerzy.waldorff@gmail.com>
2014-03-29 01:06:39 +0000

    Failing test added to issue with sets permutation

--- src/GG.Combinatorics.Tests/SetPartitionTests.cs
+++ src/GG.Combinatorics.Tests/SetPartitionTests.cs
@@ -79,6 +79,25 @@ namespace GG.Combinatorics.Tests
         }
 
         [TestMethod]
+        public void NextSetPartitionFromABandCDWillPermutateToACandBD()
+        {
+            // Given
+            var list1 = Any.CharIList("a,b");
+            var list2 = Any.CharIList("c,d");
+            var listOfLists = new List<IList<char>> { list1, list2 };
+            var expectedList1 = Any.CharIList("a,c");
+            var expectedList2 = Any.CharIList("b,d");
+
+            // When
+            NextSetPartition<char>(listOfLists);
+
+            // Then
+            Assert.AreEqual(2, listOfLists.Count);
+            CollectionAssert.AreEqual(expectedList1.ToList(), listOfLists[0].ToList());
+            CollectionAssert.AreEqual(expectedList2.ToList(), listOfLists[1].ToList());
+        }
+
+        [TestMethod]
         public void CreateIntPartitionFromTwoThreeElementSetsReturns33()
         {
             // Given
