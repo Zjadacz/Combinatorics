4f63ff5397044cd5284d53136109f4ea7c3ddd59
GG <grzegorz.godlewski@gmail.com>
2017-03-10 13:35:44 +0100

    MathHelper class updated: comments, exception when not natural number, tests added.

---
+++ src/GG.Combinatorics.Tests/MathHelperTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GG.Combinatorics.Tests
+{
+    [TestClass]
+    public class MathHelperTests
+    {
+        [TestMethod]
+        public void FactorialFromZeroReturnsOne()
+        {
+            // Given
+            var argument = 0;
+
+            // When
+            var result = MathHelper.Factorial(argument);
+
+            // Then
+            Assert.AreEqual(result, 1);
+        }
+
+        [TestMethod]
+        public void FactorialFromOneReturnsOne()
+        {
+            // Given
+            var argument = 1;
+
+            // When
+            var result = MathHelper.Factorial(argument);
+
+            // Then
+            Assert.AreEqual(result, 1);
+        }
+
+        [TestMethod]
+        public void FactorialFromSixReturns720()
+        {
+            // Given
+            var argument = 6;
+
+            // When
+            var result = MathHelper.Factorial(argument);
+
+            // Then
+            Assert.AreEqual(result, 720);
+        }
+    }
+}
--- src/GG.Combinatorics/MathHelper.cs
+++ src/GG.Combinatorics/MathHelper.cs
@@ -1,16 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GG.Combinatorics
 {
     public class MathHelper
     {
+        /// <summary>
+        /// Returns factoral of given natural number, when number is not from natural numbers domain throws exception
+        /// </summary>
         public static long Factorial(int n)
         {
-            if(n == 1)
+            if(n < 0)
+                throw new ArgumentException("Fractoral can only operate of natural numbers domain.");
+
+            if(n == 0 || n == 1)
             {
                 return 1;
             }
